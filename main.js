4815132e752761cb0e5d0d0791855f7c0822fac5
ccmsh <cocamsh@gmail.com>
2025-02-08 13:34:45 +0900

    アイコンとAutoUpdaterうｐ

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, autoUpdater } = require('electron');
 const prompt = require('electron-prompt');
 const axios = require('axios');
 const path = require('path');
 const fs = require('fs');
+const { createWindow } = require('./window'); // ウィンドウ作成用モジュール（アプリに合わせて調整）
 
 // `.env` を確実に読み込む
 require('dotenv').config({ path: path.join(app.getAppPath(), '.env') });
 
+// GitHub Releases の URL
+const feedURL = 'https://update.electronjs.org/ccmsh/NobaTor/${process.platform}-${process.arch}';
+
 let win;
+
+// アップデートの確認
+function checkForUpdates() {
+  autoUpdater.setFeedURL(feedURL);
+  autoUpdater.checkForUpdates();
+}
 
 app.whenReady().then(() => {
     win = new BrowserWindow({
@@ -21,6 +31,36 @@ app.whenReady().then(() => {
     });
 
     win.loadFile(path.join(__dirname, 'src', 'index.html'));
+
+    // アップデートの確認
+    checkForUpdates();
+
+    // autoUpdater イベントリスナー
+    autoUpdater.on('checking-for-update', () => {
+        console.log('アップデートを確認中...');
+    });
+
+    autoUpdater.on('update-available', () => {
+        console.log('新しいアップデートがあります');
+    });
+
+    autoUpdater.on('update-not-available', () => {
+        console.log('アップデートはありません');
+    });
+
+    autoUpdater.on('error', (err) => {
+        console.error('自動アップデートエラー:', err);
+    });
+
+    autoUpdater.on('download-progress', (progressObj) => {
+        let log_message = `ダウンロード速度: ${progressObj.bytesPerSecond} - ダウンロード進捗: ${progressObj.percent}% (${progressObj.transferred} / ${progressObj.total})`;
+        console.log(log_message);
+    });
+
+    autoUpdater.on('update-downloaded', (info) => {
+        console.log('アップデートがダウンロードされました');
+        autoUpdater.quitAndInstall(); // アップデートをインストール
+    });
 });
 
 // 📩 送信メッセージを受け取り、Discord に送信
